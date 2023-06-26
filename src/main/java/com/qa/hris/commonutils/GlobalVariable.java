a8fe8fe7106ce85109f4fc8721a43a8f83291923
gem-shubhambajaj <shubham.bajaj@geminisolutions.com>
2023-06-26 12:25:50 +0530

    Code Update - Shubham

--- src/main/java/com/qa/hris/commonutils/GlobalVariable.java
+++ src/main/java/com/qa/hris/commonutils/GlobalVariable.java
@@ -5,7 +5,7 @@ public class GlobalVariable {
     public static final String baseURL1 = "https://o8np03axb9.execute-api.ap-south-1.amazonaws.com";
     public static final String baseURL2 = "https://dleae1blka.execute-api.ap-south-1.amazonaws.com";
     public static final String baseURL3 = "https://j3b5pu2uol.execute-api.ap-south-1.amazonaws.com";
-    public static final String baseURL4 = "https://9ptbnkax8j.execute-api.ap-south-1.amazonaws.com";
+    public static final String baseURL4 = "https://iy6277jmyd.execute-api.ap-south-1.amazonaws.com";
     public static final String baseURL5 = "https://uri37e63uc.execute-api.ap-south-1.amazonaws.com";
     public static final String baseURL6 = "https://0yi063ypog.execute-api.ap-south-1.amazonaws.com";
     public static final String baseURL7 = "https://n0mu6l674h.execute-api.ap-south-1.amazonaws.com";
